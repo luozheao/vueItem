86102c44c6cd56537175142a1129eba9379f2601
xiekangyan <xiekangyan@shsnc.com>
2017-09-16 14:56:02 +0800

    新增个人中心页面

--- src/info.vue
+++ src/info.vue
@@ -1,8 +1,4 @@
 <style type="text/less" scoped>
- *{
-  padding:0;
-  margin:0;
- }
  ul{
   list-style-type:none;
  }
@@ -61,30 +57,60 @@
     <div class="pInfoItem">
      <h3 class="pInfoItemTop">注册信息</h3>
      <ul class="pInfoUl">
-      <li class="pInfoLi"><span class="pInfoOption">公司名称：</span><span class="pInfoTxt">维多利亚酒店同济路店</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">姓名：</span><span class="pInfoTxt">同济路地铁店</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">登录帐号：</span><span class="pInfoTxt">wdlyjdtjd</span><el-button type="text" @click="open1">修改</el-button></li>
-      <li class="pInfoLi"><span class="pInfoOption">密码：</span><span class="pInfoTxt"></span><span class="inlineBtn" style="cursor:pointer;" onclick="showpop();">修改密码</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">公司名称：</span><span class="pInfoTxt">{{ registerMsg.name }}</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">姓名：</span><span class="pInfoTxt">{{ registerMsg.hName }}</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">登录帐号：</span><span class="pInfoTxt">{{ registerMsg.account }}</span> &nbsp;<el-button type="primary" size="mini" @click="dialogFormVisible = true">修改</el-button></li>
+      <el-dialog title="更换手机号码" :visible.sync="dialogFormVisible">
+       <el-form :model="form">
+        <el-button type="primary" @click="getNum" :loading="isGetNumLoad">点击获取验证码</el-button>
+        <el-form-item label="验证码" :label-width="formLabelWidth">
+         <el-input v-model="form.num" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="新的手机号码" :label-width="formLabelWidth">
+         <el-input v-model="form.phone" auto-complete="off"></el-input>
+        </el-form-item>
+       </el-form>
+       <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="changeNum">确 定</el-button>
+       </div>
+      </el-dialog>
+      <li class="pInfoLi"><span class="pInfoOption">密码：</span><span class="pInfoTxt"></span> &nbsp;<el-button type="primary" size="mini" @click="changePassword=true">修改密码</el-button></li>
+      <el-dialog title="修改密码" :visible.sync="changePassword">
+       <el-form :model="passwordForm">
+        <el-form-item label="旧密码" :label-width="formLabelWidth">
+         <el-input v-model="passwordForm.oldPassword" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="新密码" :label-width="formLabelWidth">
+         <el-input v-model="passwordForm.newPassword" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认新密码" :label-width="formLabelWidth">
+         <el-input v-model="passwordForm.aglinNewPassword" auto-complete="off"></el-input>
+        </el-form-item>
+       </el-form>
+       <div slot="footer" class="dialog-footer">
+        <el-button @click="changePassword = false">取 消</el-button>
+        <el-button type="primary" @click="changePasswordFn">确 定</el-button>
+       </div>
+      </el-dialog>
+     </ul>
+    </div>
+    <div class="pInfoItem">
+     <h3 class="pInfoItemTop">账户信息</h3>
+     <ul class="pInfoUl">
+      <li class="pInfoLi"><span class="pInfoOption">会员状态：</span><span class="pInfoTxt">{{accountMsg.account}}</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">会员到期时间：</span><span class="pInfoTxt">{{accountMsg.endTime}}</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">续费会员：</span><span class="pInfoTxt"></span><span class="pInfoTxtRed">{{accountMsg.renew}}</span><a style="display:none;" href="Pay/openVip.aspx" class="inlineBtn">点击支付</a></li>
      </ul>
     </div>
     <div class="pInfoItem">
      <h3 class="pInfoItemTop">功能状态</h3>
      <ul class="pInfoUl">
-      <li class="pInfoLi"><span class="pInfoOption">可建区域数量：</span><span class="pInfoTxt">2个</span><span class="pInfoTxtRed">（如需更多请咨询客服电话）</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">可建问卷数量：</span><span class="pInfoTxt">50个</span><span class="pInfoTxtRed">（如需更多请咨询客服电话）</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">领导微信提醒：</span><span class="pInfoTxt">正常</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">可查看统计功能：</span><span class="pInfoTxt">正常</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">区域位置设置：</span><span class="pInfoTxt">正常</span></li>
-     </ul>
-    </div>
-    <div class="pInfoItem">
-     <h3 class="pInfoItemTop">功能状态</h3>
-     <ul class="pInfoUl">
-      <li class="pInfoLi"><span class="pInfoOption">可建区域数量：</span><span class="pInfoTxt">2个</span><span class="pInfoTxtRed">（如需更多请咨询客服电话）</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">可建问卷数量：</span><span class="pInfoTxt">50个</span><span class="pInfoTxtRed">（如需更多请咨询客服电话）</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">领导微信提醒：</span><span class="pInfoTxt">正常</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">可查看统计功能：</span><span class="pInfoTxt">正常</span></li>
-      <li class="pInfoLi"><span class="pInfoOption">区域位置设置：</span><span class="pInfoTxt">正常</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">可建区域数量：</span><span class="pInfoTxt">{{abilityMsg.area}}</span><span class="pInfoTxtRed">（如需更多请咨询客服电话）</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">可建问卷数量：</span><span class="pInfoTxt">{{abilityMsg.question}}</span><span class="pInfoTxtRed">（如需更多请咨询客服电话）</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">领导微信提醒：</span><span class="pInfoTxt">{{abilityMsg.Prompt}}</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">可查看统计功能：</span><span class="pInfoTxt">{{abilityMsg.statistics}}</span></li>
+      <li class="pInfoLi"><span class="pInfoOption">区域位置设置：</span><span class="pInfoTxt">{{abilityMsg.areaSet}}</span></li>
      </ul>
     </div>
    </div>
@@ -97,36 +123,78 @@
 //        props: ['param', 'data'],
         data() {
             return  {
-
+                registerMsg:{
+                    name:'',
+                    hName:'',
+                    account:''
+                },
+                accountMsg:{
+                    account:'',
+                    endTime:'',
+                    renew:''
+                },
+                abilityMsg:{
+                    area:'',
+                    question:'',
+                    Prompt:'',
+                    statistics:'',
+                    areaSet:''
+                },
+                isGetNumLoad:false,
+                dialogFormVisible: false,
+                changePassword: false,
+                form: {
+                    num: '',
+                    phone:''
+                },
+                passwordForm:{
+                    oldPassword:'',
+                    newPassword:'',
+                    aglinNewPassword:'',
+                },
+                formLabelWidth: '120px'
             }
         },
         methods: {
             init(){
-
+                this.registerMsg={
+                    name:'维多利亚酒店同济路店',
+                    hName:'同济路地铁店',
+                    account:'wdlyjdtjd'
+                };
+                this.accountMsg={
+                    account:'是',
+                    endTime:'2022-07-06 00:00',
+                    renew:'（请联系客服续费！）'
+                },
+                this.abilityMsg={
+                    area:'2个',
+                    question:'50个',
+                    Prompt:'正常',
+                    statistics:'正常',
+                    areaSet:'正常'
+                }
             },
-            open1() {
-                const h = this.$createElement;
-                this.$msgbox({
-                    title: '消息',
-                    message: h('p', null, [
-                        h('span', null, '内容可以是 '),
-                        h('i', { style: 'color: teal' }, 'VNode')
-                    ]),
-                    showCancelButton: true,
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    beforeClose: (action, instance, done) => {
-                        if (action === 'confirm') {
-                             done();
-                        } else {
-                            done();
-                        }
-                    }
-                }).then(action => {
-
-                });
+            changeNum(){
+                this.dialogFormVisible = false
+                console.log('账户已改');
             },
-        }
+            changePasswordFn(){
+                this.changePassword = false
+                console.log('密码已改');
+            },
+            getNum(){
+                this.isGetNumLoad=true;
+                let self=this;
+                setTimeout(function () {
+                    self.isGetNumLoad=false;
+                },5000)
+                console.log('验证码是111');
+            }
+        },
+        created() {
+            this.init();
+        },
 
     }
 </script>
