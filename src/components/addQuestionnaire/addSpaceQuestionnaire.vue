f8cfdb83b387f0ea29a5054ff0c8996dcd1c1d2d
luozheao <815149054@qq.com>
2017-10-12 01:02:22 +0800

    完成选择问卷模板

--- src/components/addQuestionnaire/addSpaceQuestionnaire.vue
+++ src/components/addQuestionnaire/addSpaceQuestionnaire.vue
@@ -20,7 +20,7 @@
 <script>
     import $ from 'jquery';
     export default {
-        props:['QName'],
+        props:['QName','tqid'],
         components: {},
         data() {
             return {}
@@ -43,6 +43,8 @@
 
             //赋值问卷名
             window.QName=this.QName;
+            //问卷数据id
+            window.tqid=this.tqid;
         },
     }
 </script>
